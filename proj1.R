1b3197022548f6969901fc9cfd73b307f5d75def
Sean <smitchell36@csu.fullerton.edu>
2021-06-23 14:01:37 -0700

    minor data cleaning

--- proj1.R
+++ proj1.R
@@ -2,5 +2,8 @@ library(tidyverse)
 
 covid <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 demographics <- read_csv("demographics.csv")
-bed <- read_csv("hospitalbeds.csv")
+beds <- read_csv("hospitalbeds.csv")
+
+# Clean data up
 covid <- covid %>% select(-Lat, -Long) %>% pivot_longer(!c(`Country/Region`,`Province/State`), names_to = 'date', values_to = 'cases')
+demographics <- demographics %>% select(-`Country Code`)
