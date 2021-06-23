6192cccff516ebf17578c20333ed9e41b8680fcd
Sean <smitchell36@csu.fullerton.edu>
2021-06-23 13:11:32 -0700

    first commit

---
+++ proj1.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+covid <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+demographics <- read_csv("demographics.csv")
+# bed <- read_csv("")
+
