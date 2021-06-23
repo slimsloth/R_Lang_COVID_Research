14738b52555e8036c1236c2544eb7292631dd091
Sean <smitchell36@csu.fullerton.edu>
2021-06-23 13:47:50 -0700

    hospital beds

--- proj1.R
+++ proj1.R
@@ -2,5 +2,5 @@ library(tidyverse)
 
 covid <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 demographics <- read_csv("demographics.csv")
-# bed <- read_csv("")
-covid <- covid %>% select(-`Province/State`, -Lat, -Long) %>% pivot_longer(!`Country/Region`, names_to = 'date', values_to = 'cases')
+bed <- read_csv("hospitalbeds.csv")
+covid <- covid %>% select(-Lat, -Long) %>% pivot_longer(!c(`Country/Region`,`Province/State`), names_to = 'date', values_to = 'cases')
