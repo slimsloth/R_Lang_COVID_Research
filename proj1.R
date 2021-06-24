d86e75a970ca942e359ed543c55caf61a7e65169
Ernie <hooghkirk.e@gmail.com>
2021-06-24 14:18:17 -0700

    completed tidy

--- proj1.R
+++ proj1.R
@@ -1,19 +1,28 @@
 library(tidyverse)
 
-covid <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+covid_cases <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+covid_deaths <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 demographics <- read_csv("demographics.csv")
 beds <- read_csv("hospitalbeds.csv")
 
 # Clean data up
-covid <- covid %>% select(-Lat, -Long) %>% pivot_longer(!c(`Country/Region`,`Province/State`), names_to = 'date', values_to = 'cases')
-covid <- covid %>% group_by(`Country/Region`,`date`) %>% summarise(cases=sum(cases))
-covid <- covid %>% group_by(`Country/Region`) %>% summarise(cases=max(cases))
-covid <- covid %>% rename(`Country` = `Country/Region`)
+covid_cases <- covid_cases %>% select(-Lat, -Long) %>% pivot_longer(!c(`Country/Region`,`Province/State`), names_to = 'date', values_to = 'cases')
+covid_cases <- covid_cases %>% group_by(`Country/Region`,`date`) %>% summarise(cases=sum(cases))
+covid_cases <- covid_cases %>% group_by(`Country/Region`) %>% summarise(cases=max(cases))
+covid_cases <- covid_cases %>% rename(`Country` = `Country/Region`)
+
+covid_deaths <- covid_deaths %>% select(-Lat, -Long) %>% pivot_longer(!c(`Country/Region`,`Province/State`), names_to = 'date', values_to = 'deaths')
+covid_deaths <- covid_deaths %>% group_by(`Country/Region`,`date`) %>% summarise(deaths=sum(deaths))
+covid_deaths <- covid_deaths %>% group_by(`Country/Region`) %>% summarise(deaths=max(deaths))
+covid_deaths <- covid_deaths %>% rename(`Country` = `Country/Region`)
 
 beds.tidy <- beds %>% group_by(Country) %>% summarize(Year=max(Year))
 beds.tidy <- beds.tidy %>% left_join(beds)
+
 demographics <- demographics %>% rename(`Country` = `Country Name`)
 demographics <- demographics %>% select(-"Series Name", -`Country Code`) %>% pivot_wider(names_from = "Series Code", values_from = "YR2015")
-# TODO - join all 3 tables
 
-joined_data <- covid %>% inner_join(beds.tidy) %>% inner_join(demographics) %>% select(-Year) %>% rename(beds = `Hospital beds (per 10 000 population)`)
+joined_data <- covid_cases %>% inner_join(beds.tidy) %>% inner_join(covid_deaths) %>% inner_join(demographics) %>% select(-Year) %>% rename(beds = `Hospital beds (per 10 000 population)`)
+joined_data <- joined_data %>% mutate(deathrate = deaths/cases) %>% view()
+joined_data <- joined_data %>% select(Country, deathrate, cases, beds, SP.DYN.LE00.IN, SP.URB.TOTL, SP.POP.TOTL, SP.POP.80UP.FE, SP.POP.80UP.MA, SP.POP.1564.MA.IN, SP.POP.1564.FE.IN, SP.POP.0014.MA.IN, SP.POP.0014.FE.IN, SP.DYN.AMRT.FE, SP.DYN.AMRT.MA, SP.POP.TOTL.FE.IN, SP.POP.TOTL.MA.IN, SP.POP.65UP.FE.IN, SP.POP.65UP.MA.IN)
+joined_data %>% view()
