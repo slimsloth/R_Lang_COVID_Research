054c1a5cade6ac84952f88d83f6fb477e0ae1196
Sean <smitchell36@csu.fullerton.edu>
2021-06-28 13:34:40 -0700

    recleaned demographics.csv

--- proj1.R
+++ proj1.R
@@ -6,6 +6,12 @@ beds <- read_csv("hospitalbeds.csv")
 
 # Clean data up
 covid <- covid %>% select(-Lat, -Long) %>% pivot_longer(!c(`Country/Region`,`Province/State`), names_to = 'date', values_to = 'cases')
-covid <- covid %>% group_by(`Country/Region`) %>% summarise(cases=max(cases))
+
+covid$date <- as.Date(covid$date,"%d%b%Y")
+class(covid$date)
+# covid %>% filter(date==max(date)) %>% view()
+#covid <- covid %>% group_by(`Country/Region`) %>% summarise(cases=max(cases))
+
+beds %>% group_by(Country) %>% summarise(beds=`Hospital beds (per 10 000 population)`)
 demographics <- demographics %>% select(-`Country Code`)
 # TODO - join all 3 tables
