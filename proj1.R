967b2a0f23692ed89045ad4c70f1da874be9f4fc
Sean <smitchell36@csu.fullerton.edu>
2021-06-28 13:43:57 -0700

    recleaned data

--- proj1.R
+++ proj1.R
@@ -21,7 +21,22 @@ beds.tidy <- beds.tidy %>% left_join(beds)
 
 demographics <- demographics %>% rename(`Country` = `Country Name`)
 demographics <- demographics %>% select(-"Series Name", -`Country Code`) %>% pivot_wider(names_from = "Series Code", values_from = "YR2015")
+demographics <- demographics %>% mutate(SP.POP.80UP = SP.POP.80UP.FE + SP.POP.80UP.MA)
+demographics <- demographics %>% mutate(SP.POP.1564.IN = SP.POP.1564.FE.IN + SP.POP.1564.MA.IN)
+demographics <- demographics %>% mutate(SP.POP.0014.IN = SP.POP.0014.FE.IN + SP.POP.0014.MA.IN)
+demographics <- demographics %>% select(Country, SP.DYN.LE00.IN, SP.URB.TOTL, SP.POP.TOTL, SP.POP.80UP, SP.POP.1564.IN, SP.POP.0014.IN)
 
 joined_data <- covid_cases %>% inner_join(beds.tidy) %>% inner_join(covid_deaths) %>% inner_join(demographics) %>% select(-Year) %>% rename(beds = `Hospital beds (per 10 000 population)`)
-joined_data <- joined_data %>% mutate(deathrate = deaths/cases) %>% view()
+joined_data <- joined_data %>% mutate(deathrate = deaths/cases)
 joined_data <- joined_data %>% relocate(deathrate, .before = cases) %>% select(-deaths)
+
+# Dependent variable: deathrate
+# Predictor variables:
+# 1. cases
+# 2. beds
+# 3. SP.DYN.LE00.IN
+# 4. SP.URB.TOTL
+# 5. SP.POP.TOTL
+# 6. SP.POP.80UP
+# 7. SP.POP.1564.IN
+# 8. SP.POP.0014.IN
