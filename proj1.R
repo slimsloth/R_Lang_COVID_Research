ed5449602922ed8cc5ded492ae6ca3db9599572a
Ernie <hooghkirk.e@gmail.com>
2021-07-01 17:10:23 -0700

    complete!

--- proj1.R
+++ proj1.R
@@ -19,10 +19,7 @@ covid_deaths <- covid_deaths %>% rename(`Country` = `Country/Region`)
 
 beds.tidy <- beds %>% group_by(Country) %>% summarize(Year=max(Year))
 beds.tidy <- beds.tidy %>% left_join(beds)
-beds.tidy <- beds.tidy %>% mutate(Country = replace(Country, Country == "Republic of Korea", "Korea, South"))
-beds.tidy <- beds.tidy %>% mutate(Country = replace(Country, Country == "Iran (Islamic Republic of)", "Iran"))
-beds.tidy <- beds.tidy %>% mutate(Country = replace(Country, Country == "United Kingdom of Great Britain and Northern Ireland", "United Kingdom"))
-
+ 
 demographics <- demographics %>% rename(`Country` = `Country Name`)
 demographics <- demographics %>% select(-"Series Name", -`Country Code`) %>% pivot_wider(names_from = "Series Code", values_from = "YR2015")
 demographics <- demographics %>% mutate(SP.POP.80UP = SP.POP.80UP.FE + SP.POP.80UP.MA)
@@ -34,8 +31,9 @@ demographics <- demographics %>% mutate(Country = replace(Country, Country == "I
 
 joined_data <- covid_cases %>% inner_join(beds.tidy) %>% inner_join(covid_deaths) %>% inner_join(demographics) %>% select(-Year) %>% rename(beds = `Hospital beds (per 10 000 population)`)
 joined_data <- joined_data %>% mutate(deathrate = deaths/cases)
-joined_data <- joined_data %>% relocate(deathrate, .before = cases) %>% select(-deaths)
-
+joined_data <- joined_data %>% relocate(deathrate, .before = cases)
+joined_data <- joined_data %>% relocate(deaths, .before = beds)
+# joined_data <- joined_data %>% relocate(deathrate, .before = cases) %>% select(-deaths)
 
 # PART 2
 # Dependent variable: deathrate
@@ -49,7 +47,26 @@ joined_data <- joined_data %>% relocate(deathrate, .before = cases) %>% select(-
 # 7. SP.POP.1564.IN
 # 8. SP.POP.0014.IN
 
-mod <- lm(data=joined_data, deathrate~SP.POP.80UP+beds)
-mycf <- coef(mod)
-ggplot(data=joined_data)+geom_point(aes(x=SP.POP.80UP, y=deathrate)) + geom_abline(intercept=mycf[1], slope=mycf[2])
-ggplot(data=joined_data, aes(x=cases, y=deathrate)) + geom_point()
+mod_A <- lm(data=joined_data, deathrate~SP.POP.80UP+beds+SP.POP.TOTL)
+mod_B <- lm(data=joined_data, deathrate~SP.POP.80UP+beds)
+mod_C <- lm(data=joined_data, deathrate~beds)
+mod_D <- lm(data=joined_data, deathrate~SP.DYN.LE00.IN)
+mod_E <- lm(data=joined_data, deathrate~SP.POP.TOTL)
+
+#summary(mod_A) 0.06514
+#summary(mod_B) 0.05249
+#summary(mod_C) 0.01097
+#summary(mod_D) 0.01045
+#summary(mod_E) 0.01809
+
+ggplot(data=joined_data)+geom_point(aes(x=deaths, cases))
+
+r_squared_mod_values <- c(0.06514, 0.05249, 0.01097, 0.01045, 0.01809)
+group <- LETTERS[1:5]
+barplot(r_squared_mod_values, xlab = "Linear Models", ylab="R^2 values", names.arg = group)
+                                          
+#plot the axis on the log scale
+# mycf <- coef(mod)
+# ggplot(data=joined_data)+geom_point(aes(x=SP.POP.80UP, y=deathrate)) + geom_abline(intercept=mycf[1], slope=mycf[2])
+# ggplot(data=joined_data, aes(x=cases, y=deathrate)) + geom_point()
+# joined_data %>% view()
